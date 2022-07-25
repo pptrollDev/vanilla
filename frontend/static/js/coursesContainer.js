c5eb390d44a994b9bd510eb3b101ff0ad150548e
sungchan <pptroll@gmail.com>
2022-07-25 10:41:36 +0900

    fix: router without express

--- frontend/static/js/coursesContainer.js
+++
@@ -1,9 +0,0 @@
-const init = () => {
-  console.log("course");
-  const course = document.querySelector(".course");
-  course.addEventListener("click", () => {
-    console.log("js works");
-  });
-};
-
-export default init;
