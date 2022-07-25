7f33e3b25e5a5c23f40f1438f83ab5c7008f80ee
sungchan <pptroll@gmail.com>
2022-07-25 11:33:26 +0900

    feat: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const { resolve } = require('path');
+
+module.exports = {
+  entry: {
+    app: './app.js'
+  },
+  output: {
+    path: resolve(__dirname, './dist'),
+    filename: '[name].js'
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'pages/404.html',
+      template: 'pages/404.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'pages/about.html',
+      template: 'pages/about.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'pages/index.html',
+      template: 'pages/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'pages/lorem.html',
+      template: 'pages/lorem.html'
+    }),
+    new MiniCssExtractPlugin({ filename: 'app.css' }),
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['dist']
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  }
+};
